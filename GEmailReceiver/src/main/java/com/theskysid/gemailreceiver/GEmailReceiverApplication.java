8790c0679576a8ab44091241b0150f90aa0905c1
theskysid <23x10sid@gmail.com>
2025-08-13 16:37:48 +0530

    backend module for gmail initialized

---
+++ GEmailReceiver/src/main/java/com/theskysid/gemailreceiver/GEmailReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.theskysid.gemailreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GEmailReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GEmailReceiverApplication.class, args);
+	}
+
+}
